0d86e3cab6df2e47e7d440dcaf35d0f7870ed13d
Michael Scalzetti <contact@scalz.cloud>
2024-12-11 18:08:29 -0500

    switching to regex

--- parse.py
+++ parse.py
@@ -1,33 +1,88 @@
-def parse_trim_start(raw_data):
-    for i in range(len(raw_data)):
-        cur_byte = raw_data[i].to_bytes(1,"big")
-        if cur_byte != b'\x00' and cur_byte!= b"\x10":
-            return raw_data[i:]
-    return b""
+# Lots of this is based on https://www.magtek.com/content/documentationfiles/d99800004.pdf
 
-def parse_trim_end(raw_data):
-    index = 0
-    for i in range(len(raw_data)):
-        if raw_data[i].to_bytes(1,"big") != b'\x00':
-            index = i
-    return raw_data[:index+1]
+import re
 
+def get_track_1(raw_data):
+
+    # Define sentinels and seperators
+    SS = b'%'
+    FS = b'^'
+    ES = b'?'
+    # [ {SS} PAN:(FormatCode - 19 Chars) {FS} NAME:(26 Chars) {FS} DATA:(29 Chars) {ES} {LRC} ]
+
+    regex_pattern = r"%(.{0,19})\^(.{0,26})\^(.{0,29})\?"
+    matches = re.findall(regex_pattern, raw_data.decode())
+    if len(matches) == 0:
+        return None
+    for match in matches:
+        temp = list(match)
+        data = {}
+        #data["LRC"] = temp.pop()
+        data["data"] = temp.pop()
+        data["name"] = temp.pop()
+        data["pan"] = temp.pop()
+        return data
+
+def get_track_2(raw_data):
+
+    # Define sentinels and seperators
+    SS = b';'
+    FS = b'='
+    ES = b'?'
+    # [ {SS} PAN:(FormatCode - 19 Chars) {FS} DATA:(17 Chars) {ES} {LRC} ]
+
+    regex_pattern = r";(.{0,19})=(.{0,17})\?"
+    matches = re.findall(regex_pattern, raw_data.decode())
+    if len(matches) == 0:
+        return None
+    for match in matches:
+        temp = list(match)
+        data = {}
+        #data["LRC"] = temp.pop()
+        data["data"] = temp.pop()
+        data["pan"] = temp.pop()
+        return data
+
+
+def get_track_3(raw_data):
+
+    # Define sentinels and seperators
+    SS = b';'
+    FS = b'='
+    ES = b'?'
+    # [ {SS} PAN:(FormatCode - 19 Chars) {FS} DATA:(84 Chars) {ES} {LRC} ]
+
+    regex_pattern = r";(.{0,19})=(.{0,84})\?(.*)$"
+    matches = re.findall(regex_pattern, raw_data.decode())
+    if len(matches) == 0:
+        return None
+    for match in matches:
+        temp = list(match)
+        data = {}
+        data["LRC"] = temp.pop()
+        data["data"] = temp.pop()
+        data["pan"] = temp.pop()
+        return data
 
 
 def get_tracks_from_raw_data(raw_data):
-    SPLIT_CHAR = b"?"
-
-    raw_data = parse_trim_end(parse_trim_start(raw_data))
-    tracks = [parse_trim_end(parse_trim_start(track)) for track in raw_data.split(SPLIT_CHAR)]
-
-    for track in tracks:
-        print(f"\t{track}")
-
-    return tracks
+    return [get_track_1(raw_data),get_track_2(raw_data),get_track_3(raw_data)]
 
 
 def parse_raw_data(raw_data):
+    raw_data = raw_data.replace(b'\x00',b'')
     tracks = get_tracks_from_raw_data(raw_data)
+    card_data = make_track_dict()
+
+    tracknum = ["Track1", "Track2", "Track3"]
+    for i in range(len(tracknum)):
+        card_data[tracknum[i]] = tracks[i]
+        print(tracks[i])
+
+    return card_data
+
+
+def make_track_dict():
     card_data = {}
 
     for item in ["Track1", "Track2", "Track3"]:
@@ -36,14 +91,17 @@ def parse_raw_data(raw_data):
     # T1
     card_data["Track1"]["PAN"] = ""
     card_data["Track1"]["NAME"] = ""
-    card_data["Track1"]["DATA"] = ""
+    card_data["Track1"]["DATA"] = []
+    card_data["Track1"]["POS"] = {}
 
     # T2
     card_data["Track2"]["PAN"]=""
-    card_data["Track2"]["DATA"]=""
+    card_data["Track2"]["DATA"]=[]
+    card_data["Track2"]["POS"] = {}
 
     # T3
     card_data["Track3"]["PAN"]= ""
-    card_data["Track3"]["DATA"]=""
-
+    card_data["Track3"]["DATA"]=[]
+    card_data["Track3"]["POS"] = {}
     return card_data
+
