1be4a24ff6ed4545c3be3b3820a16764f899ff64
Michael Scalzetti <contact@scalz.cloud>
2024-12-11 18:18:54 -0500

    completed regex conversion

--- parse.py
+++ parse.py
@@ -10,8 +10,8 @@ def get_track_1(raw_data):
     ES = b'?'
     # [ {SS} PAN:(FormatCode - 19 Chars) {FS} NAME:(26 Chars) {FS} DATA:(29 Chars) {ES} {LRC} ]
 
-    regex_pattern = r"%(.{0,19})\^(.{0,26})\^(.{0,29})\?"
-    matches = re.findall(regex_pattern, raw_data.decode())
+    regex_pattern = rb"%(.{0,19})\^(.{0,26})\^(.{0,29})\?"
+    matches = re.findall(regex_pattern, raw_data)
     if len(matches) == 0:
         return None
     for match in matches:
@@ -31,8 +31,8 @@ def get_track_2(raw_data):
     ES = b'?'
     # [ {SS} PAN:(FormatCode - 19 Chars) {FS} DATA:(17 Chars) {ES} {LRC} ]
 
-    regex_pattern = r";(.{0,19})=(.{0,17})\?"
-    matches = re.findall(regex_pattern, raw_data.decode())
+    regex_pattern = rb";(.{0,19})=(.{0,17})\?"
+    matches = re.findall(regex_pattern, raw_data)
     if len(matches) == 0:
         return None
     for match in matches:
@@ -52,8 +52,8 @@ def get_track_3(raw_data):
     ES = b'?'
     # [ {SS} PAN:(FormatCode - 19 Chars) {FS} DATA:(84 Chars) {ES} {LRC} ]
 
-    regex_pattern = r";(.{0,19})=(.{0,84})\?(.*)$"
-    matches = re.findall(regex_pattern, raw_data.decode())
+    regex_pattern = rb";(.{0,19})=(.{0,84})\?(.{0,3})$"
+    matches = re.findall(regex_pattern, raw_data)
     if len(matches) == 0:
         return None
     for match in matches:
@@ -70,38 +70,17 @@ def get_tracks_from_raw_data(raw_data):
 
 
 def parse_raw_data(raw_data):
-    raw_data = raw_data.replace(b'\x00',b'')
+    #raw_data = raw_data.replace(b'\x00',b'')
     tracks = get_tracks_from_raw_data(raw_data)
-    card_data = make_track_dict()
+    card_data={}
+    card_data["3"] = tracks.pop()
+    card_data["2"] = tracks.pop()
+    card_data["1"] = tracks.pop()
 
-    tracknum = ["Track1", "Track2", "Track3"]
-    for i in range(len(tracknum)):
-        card_data[tracknum[i]] = tracks[i]
-        print(tracks[i])
+    #tracknum = ["Track1", "Track2", "Track3"]
+    #for i in range(len(tracknum)):
+    #    card_data[tracknum[i]] = tracks[i]
+    #    print(tracks[i])
 
-    return card_data
-
-
-def make_track_dict():
-    card_data = {}
-
-    for item in ["Track1", "Track2", "Track3"]:
-        card_data[item]={}
-    
-    # T1
-    card_data["Track1"]["PAN"] = ""
-    card_data["Track1"]["NAME"] = ""
-    card_data["Track1"]["DATA"] = []
-    card_data["Track1"]["POS"] = {}
-
-    # T2
-    card_data["Track2"]["PAN"]=""
-    card_data["Track2"]["DATA"]=[]
-    card_data["Track2"]["POS"] = {}
-
-    # T3
-    card_data["Track3"]["PAN"]= ""
-    card_data["Track3"]["DATA"]=[]
-    card_data["Track3"]["POS"] = {}
     return card_data
 
